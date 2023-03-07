dbfd3e558f5cbbc96ce784bc96502cda89f2bda7
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-03-07 17:03:27 -0600

    remove --verbose flag from rsync script

--- rsync-to-git.sh
+++ rsync-to-git.sh
@@ -5,8 +5,8 @@ root="$gitdir/root"
 home="$root/home/ccb012100"
 
 # --verbose --dry-run
-rsync --verbose --relative --dirs --recursive --times --progress \
+rsync --relative --dirs --recursive --times --progress \
     --files-from=files-to-sync-from-home "$HOME/" "$home"
 
-rsync --verbose --dirs --recursive --times --progress \
+rsync --dirs --recursive --times --progress \
     --files-from=files-to-sync-from-root "/" "$root"
