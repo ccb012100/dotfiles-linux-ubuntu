ab221dc4928140fe2ff8406676ce1eb12f22aff4
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-03-22 16:57:30 -0500

    fish: add zoxide

--- root/home/ccb012100/.config/fish/tools.fish
+++ root/home/ccb012100/.config/fish/tools.fish
@@ -1,31 +1,34 @@
 #!/usr/bin/env fish
 # Put homebrew first because some tools are in the homebrew path
 
-## homebrew
+# homebrew
 fish_add_path -p /home/linuxbrew/.linuxbrew/bin
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-## GPG (GNU Privacy Guard or GnuPG)
+# GPG (GNU Privacy Guard or GnuPG)
 export GPG_TTY=$(tty)
 
-## kitty
+# kitty
 fish_add_path -p $HOME/.local/kitty.app/bin
 
-## starship
+# starship
 starship init fish | source
 
-# set window title via starship
+## set window title via starship
 function set_win_title
     echo -ne "\033]0; $(realpath .) \007"
 end
 
-## nodenv
+# nodenv
 fish_add_path -p $HOME/.nodenv/bin
 eval "$(nodenv init -)"
 
-## pnpm
+# pnpm
 export PNPM_HOME="$HOME/.local/share/pnpm"
 fish_add_path -p $PNPM_HOME
 
-## set Ripgrep configuration file
+# set Ripgrep configuration file
 export RIPGREP_CONFIG_PATH="$HOME/.config/ripgreprc"
+
+# zoxide
+zoxide init fish | source
