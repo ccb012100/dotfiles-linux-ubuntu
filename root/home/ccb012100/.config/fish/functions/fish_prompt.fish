a6c153c73df191b60de2355cd782259a51694b8f
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-03-27 10:54:57 -0500

    fish: update prompt to show more git info

--- root/home/ccb012100/.config/fish/functions/fish_prompt.fish
+++ root/home/ccb012100/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,52 @@
+set -q __fish_git_prompt_show_informative_status
+or set -g __fish_git_prompt_show_informative_status
+set -q __fish_git_prompt_showcolorhints
+or set -g __fish_git_prompt_showcolorhints
+set -q __fish_git_prompt_showupstream informative
+or set -g __fish_git_prompt_showupstream informative
+
+set -q __fish_git_prompt_showdirtystate
+or set -g __fish_git_prompt_showdirtystate 1
+set -q __fish_git_prompt_showuntrackedfiles
+or set -g __fish_git_prompt_showuntrackedfiles 1
+set -q __fish_git_prompt_showcolorhints
+or set -g __fish_git_prompt_showcolorhints 1
+set -q __fish_git_prompt_color_untrackedfiles
+or set -g __fish_git_prompt_color_untrackedfiles yellow
+set -q __fish_git_prompt_char_untrackedfiles
+or set -g __fish_git_prompt_char_untrackedfiles '?'
+set -q __fish_git_prompt_color_invalidstate
+or set -g __fish_git_prompt_color_invalidstate red
+set -q __fish_git_prompt_char_invalidstate
+or set -g __fish_git_prompt_char_invalidstate '!'
+set -q __fish_git_prompt_color_dirtystate
+or set -g __fish_git_prompt_color_dirtystate blue
+set -q __fish_git_prompt_char_dirtystate
+or set -g __fish_git_prompt_char_dirtystate '*'
+set -q __fish_git_prompt_char_stagedstate
+or set -g __fish_git_prompt_char_stagedstate '✚'
+set -q __fish_git_prompt_color_cleanstate
+or set -g __fish_git_prompt_color_cleanstate green
+set -q __fish_git_prompt_char_cleanstate
+or set -g __fish_git_prompt_char_cleanstate '✓'
+set -q __fish_git_prompt_color_stagedstate
+or set -g __fish_git_prompt_color_stagedstate yellow
+set -q __fish_git_prompt_color_branch_dirty
+or set -g __fish_git_prompt_color_branch_dirty red
+set -q __fish_git_prompt_color_branch_staged
+or set -g __fish_git_prompt_color_branch_staged yellow
+set -q __fish_git_prompt_color_branch
+or set -g __fish_git_prompt_color_branch green
+set -q __fish_git_prompt_char_stateseparator
+or set -g __fish_git_prompt_char_stateseparator '⚡'
+
+set -q __fish_git_prompt_char_upstream_ahead
+or set -g __fish_git_prompt_char_upstream_ahead '+'
+set -q __fish_git_prompt_char_upstream_behind
+or set -g __fish_git_prompt_char_upstream_behind -
+set -q __fish_git_prompt_char_upstream_diverged
+or set -g __fish_git_prompt_char_upstream_diverged ' 🚧  '
+
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
 
@@ -6,25 +55,6 @@ function fish_prompt --description 'Write out the prompt'
     echo -n (pwd)
     set_color normal
 
-    set __fish_git_prompt_showupstream informative
-
-    set -q __fish_git_prompt_showdirtystate or set -g __fish_git_prompt_showdirtystate 1
-    set -q __fish_git_prompt_showuntrackedfiles or set -g __fish_git_prompt_showuntrackedfiles 1
-    set -q __fish_git_prompt_showcolorhints or set -g __fish_git_prompt_showcolorhints 1
-    set -q __fish_git_prompt_color_untrackedfiles or set -g __fish_git_prompt_color_untrackedfiles yellow
-    set -q __fish_git_prompt_char_untrackedfiles or set -g __fish_git_prompt_char_untrackedfiles '?'
-    set -q __fish_git_prompt_color_invalidstate or set -g __fish_git_prompt_color_invalidstate red
-    set -q __fish_git_prompt_char_invalidstate or set -g __fish_git_prompt_char_invalidstate '!'
-    set -q __fish_git_prompt_color_dirtystate or set -g __fish_git_prompt_color_dirtystate blue
-    set -q __fish_git_prompt_char_dirtystate or set -g __fish_git_prompt_char_dirtystate '*'
-    set -q __fish_git_prompt_char_stagedstate or set -g __fish_git_prompt_char_stagedstate '✚'
-    set -q __fish_git_prompt_color_cleanstate or set -g __fish_git_prompt_color_cleanstate green
-    set -q __fish_git_prompt_char_cleanstate or set -g __fish_git_prompt_char_cleanstate '✓'
-    set -q __fish_git_prompt_color_stagedstate or set -g __fish_git_prompt_color_stagedstate yellow
-    set -q __fish_git_prompt_color_branch_dirty or set -g __fish_git_prompt_color_branch_dirty red
-    set -q __fish_git_prompt_color_branch_staged or set -g __fish_git_prompt_color_branch_staged yellow
-    set -q __fish_git_prompt_color_branch or set -g __fish_git_prompt_color_branch green
-    set -q __fish_git_prompt_char_stateseparator or set -g __fish_git_prompt_char_stateseparator '⚡'
     fish_vcs_prompt '|%s'
     echo
 
