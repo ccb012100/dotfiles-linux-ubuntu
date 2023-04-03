8cba7d7f8c5f4b658214975405df6bed92b46603
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-04-03 08:34:31 -0500

    fish: fix prev commit

--- root/home/ccb012100/.config/fish/functions/update_packages.fish
+++ root/home/ccb012100/.config/fish/functions/update_packages.fish
@@ -1,10 +1,10 @@
 function update_packages --description 'Update various system packages'
     sudo apt update &&
         sudo apt upgrade --yes &&
+        snap refresh &&
         sudo -k &&
         brew upgrade &&
         flatpak update --assumeyes &&
-        snap refresh &&
         gh extension upgrade --all &&
         fisher update &&
         echo \t--- all done! ---
