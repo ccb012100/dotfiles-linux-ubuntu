5aef03422f6a6e8582bf333e7d8d46d28aa3d34e
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-04-09 09:48:08 -0500

    fish: add sudo command to snap/flatpak upgrades

--- root/home/ccb012100/.config/fish/functions/update_packages.fish
+++ root/home/ccb012100/.config/fish/functions/update_packages.fish
@@ -1,8 +1,8 @@
 function update_packages --description 'Update various system packages'
     sudo apt update &&
         sudo apt upgrade --yes &&
-        snap refresh &&
-        flatpak update --assumeyes &&
+        sudo snap refresh &&
+        sudo flatpak update --assumeyes &&
         sudo -k &&
         brew upgrade &&
         gh extension upgrade --all &&
