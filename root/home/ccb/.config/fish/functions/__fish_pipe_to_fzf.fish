2670e5c819572f462e11611b5644214a6f2f014a
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-07-07 10:12:42 -0500

    fish: delete files installed by fisher plugin

--- root/home/ccb/.config/fish/functions/__fish_pipe_to_fzf.fish
+++
@@ -1,4 +0,0 @@
-function __fish_pipe_to_fzf -d "Pipe the current command to fzf"
-    set -l cmd fzf
-    fish_commandline_append " &| $cmd"
-end
--- root/home/ccb/.config/fish/functions/__fish_pipe_to_ripgrep.fish
+++
@@ -1,4 +0,0 @@
-function __fish_pipe_to_ripgrep -d "Pipe the current command to ripgrep"
-    set -l cmd rg
-    fish_commandline_append " &| $cmd"
-end
