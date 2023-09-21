011904d75964926cf5f60fde0749ea7ec679912a
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-09-21 15:31:28 -0500

    use full paths for syncing

--- rsync-to-git.sh
+++ rsync-to-git.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-REPO_ROOT="$HOME/.dotfiles/root"
-REPO_USER_DIR="$REPO_ROOT/home/ccb012100"
+repo_root="$HOME/.dotfiles/root"
+repo_user_dir="$repo_root/home/ccb012100"
+files_to_sync="$HOME/.dotfiles/files-to-sync-from-home.txt"
 
 # --verbose --dry-run
 rsync --relative --dirs --recursive --times --progress \
-    --files-from=files-to-sync-from-home "$HOME/" "$REPO_USER_DIR"
-
-# rsync --dirs --recursive --times --progress \
-    # --files-from=files-to-sync-from-root "/" "$REPO_ROOT"
+    --files-from="$files_to_sync" "$HOME/" "$repo_user_dir"
