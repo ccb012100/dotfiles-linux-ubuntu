c8ece2e0316406dbd736c5a6fef42414bfe6f0de
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-09-25 14:30:03 -0500

    add file to sync from repo to local filesystem

---
+++ sync-local-configs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu
+echo WARNING! This will overwrite your local config files! Are you sure you want to continue?
+
+read -r continue
+
+case $continue in
+yes | YES | y | Y) ;;
+*)
+    echo Exiting...
+    exit 1
+    ;;
+esac
+
+REPO_ROOT="$HOME/.dotfiles/root"
+REPO_USER_DIR="$REPO_ROOT/home/ccb012100"
+CONFIG_BACKUP="$REPO_USER_DIR/.config_bak"
+
+# --verbose --dry-run \
+rsync --relative --dirs --recursive --times --progress \
+    -vv --dry-run \
+    --backup --backup-dir="$CONFIG_BACKUP" \
+    --files-from=files-to-sync.txt \
+    "$REPO_USER_DIR" "$CONFIG_BACKUP"
