a2d08e2578a5c09d37ab565c2ddba87f836acf5c
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-09-26 12:22:10 -0500

    rename file list

--- rsync-to-git.sh
+++ rsync-to-git.sh
@@ -2,8 +2,8 @@
 
 repo_root="$HOME/.dotfiles/root"
 repo_user_dir="$repo_root/home/ccb012100"
-files_to_sync="$HOME/.dotfiles/files-to-sync-from-home.txt"
+files_to_sync="$HOME/.dotfiles/files-to-sync.txt"
 
 # --verbose --dry-run
 rsync --relative --dirs --recursive --times --progress \
-    --files-from="$files_to_sync" "$HOME/" "$repo_user_dir"
+    --files-from="$files_to_sync.txt" "$HOME/" "$repo_user_dir"
