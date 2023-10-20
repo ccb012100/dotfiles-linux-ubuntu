ad64aaa3d71bb836985091aa4a4a5d2846f87065
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-10-19 19:28:11 -0500

    fish: make ~/bin first in path

--- root/home/ccb012100/.config/fish/config.fish
+++ root/home/ccb012100/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set EDITOR nvim
 
-    set PATH $PATH $HOME/bin
+    set PATH $HOME/bin $PATH
     set PATH $PATH $HOME/.local/bin
     set PATH /home/linuxbrew/.linuxbrew/opt/coreutils/libexec/gnubin $PATH
 
