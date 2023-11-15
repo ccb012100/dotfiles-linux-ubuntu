8a2b88dcecc1ab2f77078d348d76c4c7f02e2a79
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-11-15 16:37:08 -0600

    fish: use updated playlist-util syntax

--- root/home/ccb012100/.config/fish/aliases.fish
+++ root/home/ccb012100/.config/fish/aliases.fish
@@ -32,8 +32,8 @@ alias tree 'eza --tree'
 
 # use util from <https://github.com/ccb012100/playlist-util> to search
 # Spotify playlists from <https://github.com/ccb012100/playlist-search>
-alias sm 'playlist-util tsv ~/bin/albums/sorted_albums.tsv search'
-alias sma 'playlist-util tsv ~/bin/albums/all_albums.tsv search --include-playlist-name'
-alias smp 'playlist-util tsv ~/bin/albums/sorted_albums.tsv search --include-playlist-name'
+alias sm 'playlist-util search tsv ~/bin/albums/sorted_albums.tsv'
+alias sma 'playlist-util search tsv ~/bin/albums/all_albums.tsv --include-playlist-name'
+alias smp 'playlist-util search tsv ~/bin/albums/sorted_albums.tsv --include-playlist-name'
 alias sms playlist_sync
 alias smpd $HOME/bin/playlist-dbsearch.py
