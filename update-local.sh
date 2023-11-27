b7d843e7888954082be178aae83e7ce21b00bc60
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-11-27 10:14:44 -0600

    fix/update update-local.sh script

--- update-local.sh
+++ update-local.sh
@@ -4,21 +4,22 @@ set -Eeou pipefail
 # update local config files from dotfiles repo
 
 repo=$(dirname -- "$(readlink -f -- "$0")")
-repoconf="$repo"/root/home/ccb012100/
+repoconf="$repo"/root/home/ccb012100/.config
 config="$HOME"/.config
 
 case $1 in
 bash)
     cp -uv "$repoconf"/bash/{aliases,tools,functions}.sh "$config"/bash/
-    ;;
-bashrc)
     cp -uv "$repo"/root/home/ccb012100/.bashrc "$HOME"/
     ;;
 fish)
-    cp -uv "$repoconf"/fish/{abbreviations,aliases,config,tools}.fish "$config"/fish/
+    cp -uv "$repoconf"/fish/{abbreviations,aliases,config,git-abbreviations,tools}.fish "$config"/fish/
     ;;
 git)
-    cp -uv "$repoconf"/git/{aliases.gitconfig,git_wrapper.sh} "$config"/git/
+    cp -uv "$repoconf"/git/{aliases.gitconfig,git.gitconfig,git_wrapper.sh} "$config"/git/
+    ;;
+nvim)
+    cp -uv "$repoconf"/nvim/init.vim "$config"/nvim/
     ;;
 starship)
     cp -uv "$repoconf"/starship.toml "$config"/
